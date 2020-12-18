3e7db74dd0a3e7b3ab2c1da3b2f6f2307cf9080d
EltrutLikes <10eltrut1@gmail.com>
2020-12-18 14:18:03 -0500

    actually new mod?

---
+++ src/main/java/com/evoslab/elhuevo/core/ElHuevo.java
@@ -0,0 +1,41 @@
+package com.evoslab.elhuevo.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+
+@Mod("elhuevo")
+public class ElHuevo {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "elhuevo";
+    public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MOD_ID);
+    public static ElHuevo instance;
+    
+    IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+    public ElHuevo() {
+        modEventBus.addListener(this::setup);
+        modEventBus.addListener(this::doClientStuff);
+        instance = this;
+
+        REGISTRY_HELPER.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
