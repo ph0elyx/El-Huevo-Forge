ba204b6eb103d6e4df0366c285cea1e82e4011b1
EltrutLikes <10eltrut1@gmail.com>
2021-02-17 16:23:20 -0500

    walking animation?

--- src/main/java/com/evoslab/elhuevo/client/model/EgdogEntityModel.java
+++ src/main/java/com/evoslab/elhuevo/client/model/EgdogEntityModel.java
@@ -6,14 +6,19 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.util.math.MathHelper;
 
 public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 	
-	private final ModelRenderer body;
-	private final ModelRenderer leftleg;
-	private final ModelRenderer rightleg;
-	private final ModelRenderer bodyhead;
-	private final ModelRenderer tail;
+	protected T entity;
+	
+	private ModelRenderer body;
+	private ModelRenderer leftleg;
+	private ModelRenderer rightleg;
+	private ModelRenderer bodyhead;
+	private ModelRenderer tail;
+	private ModelRenderer cube_r2;
+    private ModelRenderer cube_r1;
 
 	public EgdogEntityModel() {
 		textureWidth = 32;
@@ -48,10 +53,14 @@ public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 	}
 
 	@Override
-	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {}
+	public void setRotationAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+		this.entity = entity;
+		this.leftleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.rightleg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+	}
 
 	@Override
-	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 		body.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
@@ -65,5 +74,4 @@ public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 	public void setLivingAnimations(T entityIn, float limbSwing, float limbSwingAmount, float partialTick) {
 		super.setLivingAnimations(entityIn, limbSwing, limbSwingAmount, partialTick);
 	}
-	
 }
