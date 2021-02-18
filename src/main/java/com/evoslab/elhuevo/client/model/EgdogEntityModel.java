aa267602a931ce41cdafdd1550b9b4912277bdc6
StevePlayzz <65874635+StevePlayzz@users.noreply.github.com>
2021-02-17 19:15:50 -0500

    Sitting?

--- src/main/java/com/evoslab/elhuevo/client/model/EgdogEntityModel.java
+++ src/main/java/com/evoslab/elhuevo/client/model/EgdogEntityModel.java
@@ -24,32 +24,32 @@ public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 		textureWidth = 32;
 		textureHeight = 32;
 
-		body = new ModelRenderer(this);
-		body.setRotationPoint(0.0F, 24.0F, 0.0F);
-		body.setTextureOffset(0, 18).addBox(-3.0F, -9.0F, -2.0F, 6.0F, 8.0F, 5.0F, 0.1F, false);
+		this.body = new ModelRenderer(this);
+		this.body.setRotationPoint(0.0F, 24.0F, 0.0F);
+		this.body.setTextureOffset(0, 18).addBox(-3.0F, -9.0F, -2.0F, 6.0F, 8.0F, 5.0F, 0.1F, false);
 
-		leftleg = new ModelRenderer(this);
-		leftleg.setRotationPoint(-1.5F, -1.5F, 0.5F);
-		body.addChild(leftleg);
-		leftleg.setTextureOffset(22, 6).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
+		this.leftleg = new ModelRenderer(this);
+		this.leftleg.setRotationPoint(-1.5F, -1.5F, 0.5F);
+		this.body.addChild(leftleg);
+		this.leftleg.setTextureOffset(22, 6).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
 
-		rightleg = new ModelRenderer(this);
-		rightleg.setRotationPoint(1.5F, -1.5F, 0.5F);
-		body.addChild(rightleg);
-		rightleg.setTextureOffset(22, 9).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
+		this.rightleg = new ModelRenderer(this);
+		this.rightleg.setRotationPoint(1.5F, -1.5F, 0.5F);
+		this.body.addChild(rightleg);
+		this.rightleg.setTextureOffset(22, 9).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
 
-		bodyhead = new ModelRenderer(this);
-		bodyhead.setRotationPoint(0.0F, -5.6F, -0.2F);
-		body.addChild(bodyhead);
-		bodyhead.setTextureOffset(26, 3).addBox(2.0F, -4.4F, 0.2F, 2.0F, 2.0F, 1.0F, 0.0F, false);
-		bodyhead.setTextureOffset(26, 0).addBox(-4.0F, -4.4F, 0.2F, 2.0F, 2.0F, 1.0F, 0.0F, false);
-		bodyhead.setTextureOffset(0, 13).addBox(-2.0F, -0.4F, -2.8F, 4.0F, 2.0F, 1.0F, 0.0F, false);
-		bodyhead.setTextureOffset(0, 0).addBox(-3.0F, -3.4F, -1.8F, 6.0F, 8.0F, 5.0F, 0.0F, false);
+		this.bodyhead = new ModelRenderer(this);
+		this.bodyhead.setRotationPoint(0.0F, -5.6F, -0.2F);
+		this.body.addChild(bodyhead);
+		this.bodyhead.setTextureOffset(26, 3).addBox(2.0F, -4.4F, 0.2F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		this.bodyhead.setTextureOffset(26, 0).addBox(-4.0F, -4.4F, 0.2F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		this.bodyhead.setTextureOffset(0, 13).addBox(-2.0F, -0.4F, -2.8F, 4.0F, 2.0F, 1.0F, 0.0F, false);
+		this.bodyhead.setTextureOffset(0, 0).addBox(-3.0F, -3.4F, -1.8F, 6.0F, 8.0F, 5.0F, 0.0F, false);
 
-		tail = new ModelRenderer(this);
-		tail.setRotationPoint(0.0F, 3.6F, 2.7F);
-		bodyhead.addChild(tail);
-		tail.setTextureOffset(17, 0).addBox(-1.0F, -1.0F, 0.5F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+		this.tail = new ModelRenderer(this);
+		this.tail.setRotationPoint(0.0F, 3.6F, 2.7F);
+		this.bodyhead.addChild(tail);
+		this.tail.setTextureOffset(17, 0).addBox(-1.0F, -1.0F, 0.5F, 2.0F, 2.0F, 1.0F, 0.0F, false);
 	}
 
 	@Override
@@ -61,7 +61,7 @@ public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 
 	@Override
 	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		body.render(matrixStack, buffer, packedLight, packedOverlay);
+		this.body.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
@@ -74,9 +74,9 @@ public class EgdogEntityModel<T extends EgdogEntity> extends EntityModel<T> {
 	public void setLivingAnimations(T entityIn, float limbSwing, float limbSwingAmount, float partialTick) {
 		super.setLivingAnimations(entityIn, limbSwing, limbSwingAmount, partialTick);
 		
-//		if (entityIn.isSitting()) {
-//			leftleg.setTextureOffset(22, 6).addBox(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, false);
-//		} else 
-//			leftleg.setTextureOffset(22, 6).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
+		if (entityIn.isSitting()) {
+			this.leftleg.setTextureOffset(22, 6).addBox(0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F, false);
+		} else 
+			this.leftleg.setTextureOffset(22, 6).addBox(-1.0F, 0.5F, -1.0F, 2.0F, 1.0F, 2.0F, 0.0F, false);
 	}
 }
